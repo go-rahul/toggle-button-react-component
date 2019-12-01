60124e7e989bdbc80d8643400ef3c4ce88d15573
Rahul Biswas <rahulbiswas9832@gmail.com>
2019-12-01 13:31:56 +0530

    Updates

---
+++ precache-manifest.fd7420c75dbef3834500793829e52232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8514a8a89898f204e0ecd2cd9edd33e5",
+    "url": "/react-toggle-button/index.html"
+  },
+  {
+    "revision": "485a69f9440375dcbe28",
+    "url": "/react-toggle-button/static/css/main.8e5d42f8.chunk.css"
+  },
+  {
+    "revision": "1d1b7f9e7dda84180771",
+    "url": "/react-toggle-button/static/js/2.cd53ae56.chunk.js"
+  },
+  {
+    "revision": "485a69f9440375dcbe28",
+    "url": "/react-toggle-button/static/js/main.e8db4a57.chunk.js"
+  },
+  {
+    "revision": "b9317f1b236bc9763e4a",
+    "url": "/react-toggle-button/static/js/runtime-main.b25193ef.js"
+  }
+]);
