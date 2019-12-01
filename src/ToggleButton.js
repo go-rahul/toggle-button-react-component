44c0c5fbe7cdb0b52527b07a87b1a5a5ccee4afc
Rahul Biswas <rahulbiswas9832@gmail.com>
2019-12-01 13:20:56 +0530

    first commit

---
+++ src/ToggleButton.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./ToggleButton.scss"
+
+export default class ToggleButton extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isToggleOn: false
+    }
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }))
+  }
+
+  render() {
+    return(
+      <div className="slider" onClick={ () => this.handleClick() }>
+        <div className="round-container">
+          <div className={ "round-" + (this.state.isToggleOn ? "on":"off") }>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
